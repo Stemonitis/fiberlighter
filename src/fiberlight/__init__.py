1f500a7dfe2d4d7da7d60d8a41c0ba7501126040
Stemonitis <atokhtamysh@gmail.com>
2026-08-06 17:56:47 -0700

    make changes to init py

--- src/fiberlight/__init__.py
+++ src/fiberlight/__init__.py
@@ -1,8 +1,7 @@
+from .read_input import read_csv_file
 from .interpolation_filtering import read_data_1_animal
-from .interpolation_filtering import read_data_1_animal
-from .interpolation_filtering import read_data_1_animal
 
 
 
 
-__all__ = ["read_data_1_animal"]
+__all__ = ["read_csv_file", "read_data_all_animals"]
